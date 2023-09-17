7c8bfc566c118b5db006a9e9473734de971b550d
Tokiosman80F <tokiosman96@gmail.com>
2023-09-17 22:32:20 +0600

    cors added

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express=require('express')
+const cors=require('cors')
 const app = express()
 const port=4000
 
 const chefData=require('./data/chef.json')
 const recipeData=require('./data/recipe.json')
+
+app.use(cors())
 
 app.get('/',(req,res)=>{res.send('hello express')})
 
