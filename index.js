194785c256a8800a344499feb22e1baea58f5934
Tokiosman80F <tokiosman96@gmail.com>
2023-09-17 22:24:56 +0600

    data is ready

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 const express=require('express')
 const app = express()
 const port=4000
+
+const chefData=require('./data/chef.json')
+const recipeData=require('./data/recipe.json')
+
 app.get('/',(req,res)=>{res.send('hello express')})
+
+app.get('/chef',(req,res)=>{res.send(chefData)})
+app.get('/recipe',(req,res)=>{res.send(recipeData)})
+// specific data
+app.get('/recipe/:id',(req,res)=>{
+    const id=req.params.id;
+    const filterData=recipeData.find(item=>item.id==id)
+    res.send(filterData)
+})
+
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
   })
